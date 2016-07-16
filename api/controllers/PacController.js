78fdb100d7d467b5e783df41f778a4953164cad2
梁博文 <lbw105516@alibaba-inc.com>
2016-07-17 00:09:28 +0800

    优化pac模板

--- api/controllers/PacController.js
+++ api/controllers/PacController.js
@@ -5,42 +5,38 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 'use strict';
+
+/**
+ * 渲染PAC文件
+ * @param res
+ * @param viewName
+ * @param userProxy
+ */
+let renderPac = (res, viewName, userProxy)=> {
+    if (!res) {
+        return;
+    }
+    return res.view(viewName, {userProxy: userProxy});
+};
+
 module.exports = {
     /**
      * 返回指定用户的PAC文件
      * @param req
      * @param res
      */
-    pac: function (req, res) {
+    pac: (req, res)=> {
         let userName = req.param('userName');
 
         UserProxy.findOne({
             userName: userName
-        }).exec(function (err, userProxy) {
+        }).exec((err, userProxy)=> {
             if (err) {
                 return res.negotiate(err);
             }
-            if (!userProxy) {
 
-                UserProxy.create({
-                    userName: userName,
-                    proxyHost: '1.2.3.4',
-                    proxyPort: '9999'
-                }).exec(function createCB(err, created) {
-
-                    //渲染PAC文件
-                    let host = created.proxyHost;
-                    let port = created.proxyPort;
-                    return res.view('pac', {userName: created.userName, proxyHost: host, proxyPort: port});
-                });
-            } else {
-
-
-                //渲染PAC文件
-                let host = userProxy.proxyHost;
-                let port = userProxy.proxyPort;
-                return res.view('pac', {userName: userName, proxyHost: host, proxyPort: port});
-            }
+            //渲染PAC文件
+            return renderPac(res, 'pac', userProxy);
         });
     },
 
@@ -50,7 +46,7 @@ module.exports = {
      * @param req
      * @param res
      */
-    set: function (req, res) {
+    set: (req, res)=> {
         let userName = req.param('userName');
 
         let proxyPath = req.param('proxyPath').split(':');
@@ -59,7 +55,7 @@ module.exports = {
 
         UserProxy.findOne({
             userName: userName
-        }).exec(function (err, userProxy) {
+        }).exec((err, userProxy)=> {
                 if (err) {
                     return res.negotiate(err);
                 }
